e1e16d9e98b72b2e76e63292ac330c8fd2b169c9
sofyane <sofyane-haffaf@eurequat-algerie.com>
2014-06-16 12:41:30 +0100

    ajout de la classe user dans le model

---
+++ vente_embarque.DataLayer/Entities/XpoUser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DevExpress.Xpo;
+
+namespace vente_embarque.DataLayer.Entities
+{
+    public class XpoUser : XPBaseObject
+    {
+        public XpoUser()
+        {
+
+        }
+
+        public XpoUser(Session session)
+            : base(session)
+        {
+
+        }
+
+        private Guid _oid;
+        [Key]
+        public Guid Oid
+        {
+            get { return _oid; }
+            set { SetPropertyValue("Oid", ref _oid, value); }
+        }
+
+        private String _idUser;
+        public String IdUser
+        {
+            get { return _idUser; }
+            set { SetPropertyValue("Name", ref _idUser, value); }
+        }
+
+        private String _login;
+        public String Login
+        {
+            get { return _login; }
+            set { SetPropertyValue("Name", ref _login, value); }
+        }
+
+        private String _password;
+        public String Password
+        {
+            get { return _password; }
+            set { SetPropertyValue("Name", ref _password, value); }
+        }
+    }
+}
---
+++ vente_embarque.Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using vente_embarque.Core.Domain;
+
+namespace vente_embarque.Model
+{
+    public class User:EntityBase<Guid>
+    {
+        public int IdUser { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        
+        protected override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class FactoryUser
+    {
+        public static User CreateUser(int idUser, string login, string password)
+        {
+            var user = new User { id = Guid.NewGuid(), IdUser = idUser, Login = login, Password = password, newObject = true };
+            return user;
+        }
+    }
+}
